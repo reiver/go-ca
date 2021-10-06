314711520634a0a820523bd57b87f3e32e64594a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-10-06 15:22:03 -0700

    krnl

---
+++ krnl/kernel_parse_base64.go
@@ -0,0 +1,35 @@
+package krnl
+
+import (
+	"encoding/base64"
+	"strings"
+	"io"
+	"io/ioutil"
+)
+
+func (receiver *Kernel) parse_base64(src string) error {
+	if nil == receiver {
+		return errNilReceiver
+	}
+
+	var reader io.Reader = strings.NewReader(src)
+	if nil == reader {
+		return errInternalError
+	}
+
+	var decoder io.Reader = base64.NewDecoder(base64.StdEncoding, reader)
+	if nil == decoder {
+		return errInternalError
+	}
+
+	p, err :=  ioutil.ReadAll(decoder)
+	if nil != err {
+		return err
+	}
+
+	if err := receiver.UnmarshalBinary(p); nil != err {
+		return err
+	}
+
+	return nil
+}
---
+++ krnl/kernel_parse_base64_test.go
@@ -0,0 +1,153 @@
+package krnl_test
+
+import (
+	"github.com/reiver/go-ca/krnl"
+
+	"bytes"
+
+	"testing"
+)
+
+func TestKernel_Parse_base64_success(t *testing.T) {
+
+	tests := []struct{
+		Src string
+		Expected []byte
+	}{
+		{
+			Src: "",
+			Expected: []byte{},
+		},
+
+
+
+		{
+			Src: "SGVsbG8gd29ybGQh",
+			Expected: []byte("Hello world!"),
+		},
+
+
+
+		{
+			Src: "YXBwbGU=",
+			Expected: []byte("apple"),
+		},
+		{
+			Src: "YmFuYW5h",
+			Expected: []byte("banana"),
+		},
+		{
+			Src: "Y2hlcnJ5",
+			Expected: []byte("cherry"),
+		},
+
+
+
+		{
+			Src: "QQ==",
+			Expected: []byte("A"),
+		},
+		{
+			Src: "QUI=",
+			Expected: []byte("AB"),
+		},
+		{
+			Src: "QUJD",
+			Expected: []byte("ABC"),
+		},
+		{
+			Src: "QUJDRA==",
+			Expected: []byte("ABCD"),
+		},
+		{
+			Src: "QUJDREU=",
+			Expected: []byte("ABCDE"),
+		},
+		{
+			Src: "QUJDREVG",
+			Expected: []byte("ABCDEF"),
+		},
+		{
+			Src: "QUJDREVGRw==",
+			Expected: []byte("ABCDEFG"),
+		},
+		{
+			Src: "QUJDREVGR0g=",
+			Expected: []byte("ABCDEFGH"),
+		},
+		{
+			Src: "QUJDREVGR0hJ",
+			Expected: []byte("ABCDEFGHI"),
+		},
+		{
+			Src: "QUJDREVGR0hJSg==",
+			Expected: []byte("ABCDEFGHIJ"),
+		},
+
+
+
+		{
+			Src: "//7//v/+",
+			Expected: []byte{0xff,0xfe,0xff,0xfe,0xff,0xfe},
+		},
+	}
+
+	for testNumber, test := range tests {
+
+		var kernel krnl.Kernel
+
+		{
+			err := kernel.Parse(test.Src, "base64")
+			if nil != err {
+				t.Errorf("For test #%d, did not expect an error, but actually got one.", testNumber)
+				t.Logf("ERROR: (%T) %s", err, err)
+				t.Logf("SOURCE: %q", test.Src)
+				continue
+			}
+		}
+
+		{
+			var expected []byte = test.Expected
+			var actual   []byte = kernel.Bytes()
+
+			if !bytes.Equal(expected, actual) {
+				t.Errorf("For test #%d, the actual value of the kernel as a []byte is not what was expected.", testNumber)
+				t.Logf("EXPECTED: #%v", expected)
+				t.Logf("ACTUAL:   #%v", actual)
+				t.Logf("SOURCE: %q", test.Src)
+				continue
+			}
+		}
+	}
+}
+
+func TestKernel_Parse_base64_failure(t *testing.T) {
+
+	tests := []struct{
+		Src string
+	}{
+		{
+			Src: "<[+]>",
+		},
+
+
+
+		{
+			Src: "!@#$%^&*()_+",
+		},
+	}
+
+	for testNumber, test := range tests {
+
+		var kernel krnl.Kernel
+
+		err := kernel.Parse(test.Src, "base64")
+
+		if nil == err {
+			t.Errorf("For test #%d, expected an error but did not actually get one.", testNumber)
+			t.Logf("SOURCE: %q", test.Src)
+			t.Logf("ERROR: %s", err)
+			continue
+		}
+	}
+}
