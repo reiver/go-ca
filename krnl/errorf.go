eb8cb9859f12ff01f9da37436efbc056bf7129f7
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-10-06 17:11:58 -0700

    krnl

--- krnl/errorf.go
+++ krnl/errorf.go
@@ -5,7 +5,7 @@ import (
 )
 
 func errorf(format string, a ...interface{}) error {
-	format = fmt.Sprintf("ca: %s", format)
+	format = fmt.Sprintf("ca/krnl: %s", format)
 
 	return fmt.Errorf(format, a...)
 }
